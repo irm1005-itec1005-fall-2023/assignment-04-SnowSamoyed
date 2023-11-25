ab18962b0dce4e7d7d2fad9abd40704fc4eac0ae
Maxine Zeng <maxine.zeng76@gmail.com>
2023-11-25 07:16:33 -0500

    draft 1

--- script.js
+++ script.js
@@ -13,6 +13,9 @@
 const appID = "app";
 const headingText = "To do. To done. ✅";
 
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
 // DOM Elements
 let appContainer = document.getElementById(appID);
 
@@ -40,4 +43,63 @@ function inititialise() {
 //
 // Inits & Event Listeners
 //
-inititialise();
+// inititialise();
+
+function add_todo(){
+  if(inputBox.value === ""){
+    alert("You must write something");
+  }
+  else{
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener("click", removeTodo, false);
+
+function removeTodo(event){
+  if (event.target.tagName === "LI"){
+    event.target.classList.toggle("checked");
+    saveData();
+  }
+  // link this with the closing animation????
+  else if (event.target.tagName === "SPAN"){
+    event.target.parentElement.remove();
+    saveData();
+  }
+}
+
+// Todo Manager area -----------------------------------------------------------
+// Display number of todos (inside HTMLCollection the length is 2 but can't print out ????)
+const Count = document.getElementById("list-container").getElementsByTagName("li").length; //Not working
+const todoCount = document.getElementById("todo-count");
+
+console.log(Count);
+todoCount.innerHTML = Count;
+
+
+
+// Add clear everything
+btn_clear.addEventListener("click", clear_all, false);
+function clear_all(){
+  listContainer.innerHTML = "";
+  saveData();
+}
+
+
+function saveData(){
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showSaved(){
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showSaved();
